ed1420780a93063c78b2e07fba95cdfbeb26b1bc
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-12-13 20:17:04 +0530

    Delete Event

---
+++ server/services/event/delete.service.js
@@ -0,0 +1,19 @@
+import Event from "../../models/event.model.js";
+import { ApiError, handleServerError } from "../../utils/error.util.js";
+
+const deleteEventService = async (id) => {
+    try {
+        const event = await Event.findById(id);
+        if (!event) {
+            throw new ApiError(404, "Event not found");
+        }
+
+        await event.deleteOne();
+        return true;
+    } catch (err) {
+        console.log(err)
+        handleServerError(err);
+    }
+}
+
+export default deleteEventService
