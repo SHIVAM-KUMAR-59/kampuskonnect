327e9068648826b028f8a5d9744b68166315e5e1
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-12-13 11:54:32 +0530

    Match students and alumnis

--- server/routes/student.route.js
+++ server/routes/student.route.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import reqBodyMiddleware from "../middleware/reqBody.middleware.js";
 import authMiddleware from "../middleware/auth.middleware.js";
 import updateStudentProfileController from "../controllers/user/student/updateProfile.controller.js";
+import matchAlumniController from "../controllers/user/student/matchAlumni.controller.js";
 
 const router = Router();
 
 router.use(authMiddleware);
 router.put("/profile", reqBodyMiddleware, updateStudentProfileController);
+router.get("/match", matchAlumniController);
 
 export default router;
