6d113d410708cf57a76f39266e5a0289b0b8b319
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-12-13 20:50:43 +0530

    Get User by ID

--- server/controllers/user/getCurrentUser.controller.js
+++ server/controllers/user/getCurrentUser.controller.js
@@ -1,18 +1,8 @@
 import getCurrentUser from "../../services/user/getCurrentUser.service.js";
-import { isValidObjectId } from "../../utils/common.util.js";
 
 const getCurrentUserController = async (req, res, next) => {
-  const { id } = req.params;
-  if (!id || id.trim() === "") {
-    return res.status(400).json({ message: "User id is required", success: false });
-  }
-
-  if (!isValidObjectId(id)) {
-    return res.status(400).json({ message: "Invalid user id", success: false });
-  }
-
   try {
-    const user = await getCurrentUser(id, req.user);
+    const user = await getCurrentUser(req.user);
     return res.status(200).json({ user, success: true });
   } catch (err) {
     next(err);
