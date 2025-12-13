d4be1284c2087f210ab39d823b98194046c5305e
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-12-13 10:42:11 +0530

    Update student profile

--- server/routes/api.route.js
+++ server/routes/api.route.js
@@ -3,12 +3,14 @@ import authRoutes from "./auth.route.js";
 import userRoutes from "./user.route.js";
 import adminRoutes from "./admin.route.js";
 import eventRoutes from "./event.route.js";
+import studentRoutes from "./student.route.js";
 import errorHandlerMiddleware from "../middleware/error.middleware.js";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/user", userRoutes);
+router.use("/student", studentRoutes);
 router.use("/admin", adminRoutes);
 router.use("/event", eventRoutes);
 router.use(errorHandlerMiddleware);
---
+++ server/routes/student.route.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import reqBodyMiddleware from "../middleware/reqBody.middleware.js";
+import authMiddleware from "../middleware/auth.middleware.js";
+import updateStudentProfileController from "../controllers/user/student/updateProfile.controller.js";
+
+const router = Router();
+
+router.use(authMiddleware);
+router.put("/profile", reqBodyMiddleware, updateStudentProfileController)
+
+export default router;
