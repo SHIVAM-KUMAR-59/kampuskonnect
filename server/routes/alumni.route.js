9299828293e0efc557c71d3f66b90b445cca51e0
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-12-13 15:36:16 +0530

    Fetching all requests

--- server/routes/alumni.route.js
+++ server/routes/alumni.route.js
@@ -3,10 +3,12 @@ import reqBodyMiddleware from "../middleware/reqBody.middleware.js";
 import authMiddleware from "../middleware/auth.middleware.js";
 import updateAlumniProfileController from "../controllers/user/alumni/updateProfile.controller.js";
 import getConnectionRequestController from "../controllers/user/alumni/getConnectionRequest.controller.js";
+import alumniMiddleware from "../middleware/alumni.middleware.js";
 
 const router = Router();
 
 router.use(authMiddleware);
+router.use(alumniMiddleware);
 router.put("/profile", reqBodyMiddleware, updateAlumniProfileController);
 router.get("/requests", getConnectionRequestController);
 
--- server/services/user/alumni/getConnecionRequest.service.js
+++ server/services/user/alumni/getConnecionRequest.service.js
@@ -1,6 +1,6 @@
-import { handleServerError } from "../../../utils/error.util.js";
+import { ApiError, handleServerError } from "../../../utils/error.util.js";
 import Request from "../../../models/request.model.js";
-import { RequestStatus } from "../../../config/enums.config.js";
+import { RequestStatus, UserRole } from "../../../config/enums.config.js";
 import { mapRequest } from "../../../utils/mapResult.util.js";
 
 const getConnectionRequestService = async (userId) => {
