5009b760aeb217de7827424f17e091b6b11756bd
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-12-13 23:54:23 +0530

    Better API calling

--- client/app/dashboard/page.js
+++ client/app/dashboard/page.js
@@ -2,16 +2,22 @@
 import api from "@/utils/axios";
 import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Page = () => {
   const { data: session, status } = useSession();
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState(null);
   const fetchData = async () => {
     try {
+      setLoading(true);
       const response = await api.get("/user");
+      setUser(response.data.user);
       console.log(response.data);
     } catch (err) {
       console.log(err);
+    } finally {
+      setLoading(false);
     }
   };
   useEffect(() => {
@@ -28,12 +34,20 @@ const Page = () => {
 
   return (
     <div>
-      <p>Name: {session.user.name}</p>
-      <p>Email: {session.user.email}</p>
-      <Image src={session.user.image} alt="profile" width={100} height={100} />
-      <button className="px-3 py-2 rounded-md bg-black text-white" onClick={() => signOut()}>
-        Signout
-      </button>
+      {loading ? (
+        <p>Loading user data...</p>
+      ) : (
+        user && (
+          <>
+            <p>Name: {session.user.name}</p>
+            <p>Email: {session.user.email}</p>
+            <Image src={session.user.image} alt="profile" width={100} height={100} />
+            <button className="px-3 py-2 rounded-md bg-black text-white" onClick={() => signOut()}>
+              Signout
+            </button>
+          </>
+        )
+      )}
     </div>
   );
 };
