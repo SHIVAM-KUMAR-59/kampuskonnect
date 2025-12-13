54a16ec43ce239744e7e7d79f942e89a1a9947d6
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-12-13 15:48:18 +0530

    Handle request

---
+++ server/services/user/alumni/handlePendingRequest.service.js
@@ -0,0 +1,39 @@
+import { RequestStatus } from "../../../config/enums.config.js";
+import Request from "../../../models/request.model.js";
+import Student from "../../../models/student.model.js";
+import { handleServerError, ApiError } from "../../../utils/error.util.js";
+
+const handlePendingRequestService = async (action, requestId, user) => {
+  try {
+    if (!action === RequestStatus.ACCEPTED || !action === RequestStatus.REJECTED) {
+      throw new ApiError(400, "Invalid action");
+    }
+
+    const request = await Request.findById(requestId);
+    if (!request) {
+      throw new ApiError(404, "Request not found");
+    }
+    if (request.status !== RequestStatus.PENDING || request.status !== RequestStatus.REJECTED) {
+      throw new ApiError(400, "Request already processed");
+    }
+
+    request.status = action;
+    user.studentConnections.push(request.sender);
+
+    const sendingUser = await Student.findById(request.sender);
+    if (!sendingUser) {
+        throw new ApiError(404, "User who sent the request is deleted");
+    }
+
+    sendingUser.alumniConnections.push(user._id);
+    await sendingUser.save();
+    await user.save();
+    await request.save();
+    return true;
+  } catch (err) {
+    console.error("Error in handlePendingRequestService:", err);
+    handleServerError(err);
+  }
+};
+
+export default handlePendingRequestService;
