43b3568759c7e6449633377f38f1402a7cb7f1a8
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-12-14 20:49:37 +0530

    Fetch all chats done

--- server/routes/chat.route.js
+++ server/routes/chat.route.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import reqBodyMiddleware from "../middleware/reqBody.middleware.js";
 import authMiddleware from "../middleware/auth.middleware.js";
 import createChatController from "../controllers/chat/create.controller.js";
+import fetchAllChatsController from "../controllers/chat/fetch.controller.js";
 
 const router = Router();
 
 router.use(authMiddleware);
-router.post("/create", reqBodyMiddleware, createChatController);
+router.get("/", fetchAllChatsController);
+router.post("/", reqBodyMiddleware, createChatController);
 
 export default router;
---
+++ server/services/chat/fetch.service.js
@@ -0,0 +1,26 @@
+import Chat from "../../models/chat.schema.js";
+import { ApiError, handleServerError } from "../../utils/error.util.js";
+import { UserRole } from "../../config/enums.config.js";
+import { mapChat } from "../../utils/mapResult.util.js";
+
+const fetchAllChatsService = async (user) => {
+  try {
+    if (![UserRole.STUDENT, UserRole.ALUMNI].includes(user.role)) {
+      throw new ApiError(400, "Invalid user role");
+    }
+
+    const query = user.role === UserRole.STUDENT ? { student: user.id } : { alumni: user.id };
+
+    const chats = await Chat.find(query)
+      .populate("student", "name profileImage email")
+      .populate("alumni", "name profileImage email")
+      .populate("lastMessage")
+      .sort({ updatedAt: -1 });
+
+    return chats.map((chat) => mapChat(chat));
+  } catch (err) {
+    handleServerError(err);
+  }
+};
+
+export default fetchAllChatsService;
