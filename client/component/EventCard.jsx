a8b76d36a281732ffe7c4fa047fee381ce3aa695
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2026-03-14 09:01:07 +0530

    Minor changes

--- client/component/EventCard.jsx
+++ client/component/EventCard.jsx
@@ -1,8 +1,12 @@
+import { Link } from "lucide-react";
+
 export default function EventCard({ title, date }) {
   return (
-    <div className="border rounded-lg p-4 hover:bg-gray-50 cursor-pointer">
+    <Link href="/dashboard/events">
+    <div className="border border-neutral-200 rounded-lg p-4 hover:bg-gray-50 cursor-pointer">
       <p className="font-medium text-gray-800">{title}</p>
       <p className="text-xs text-gray-500 mt-1">{date}</p>
     </div>
+    </Link>
   );
 }
