192e04784960c01add9b863222c69165b6ce8594
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2026-03-14 09:09:12 +0530

    Minor changes

--- client/component/EventCard.jsx
+++ client/component/EventCard.jsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 export default function EventCard({ title, date }) {
   return (
-    <Link href="/dashboard/events" className="my-1">
     <div className="border border-neutral-200 rounded-lg p-4 hover:bg-gray-50 cursor-pointer">
+    <Link href="/dashboard/events">
       <p className="font-medium text-gray-800">{title}</p>
       <p className="text-xs text-gray-500 mt-1">{date}</p>
-    </div>
     </Link>
+    </div>
   );
 }
